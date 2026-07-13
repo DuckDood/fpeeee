55e8e90d59b6eb8e6a795665095a5cba814812dd
DuckDood <b.c.youtub@gmail.com>
2026-07-13 21:36:01 +1200

    improved demo

--- demos/fluid_3d.c
+++ demos/fluid_3d.c
@@ -38,6 +38,8 @@ typedef struct {
 
 	Uint64 frame_tick_count;
 	int frame_count;
+
+	Uint64 spawn_tick_count;
 } prog_state;
 
 SDL_AppResult SDL_AppInit(void **appstate, [[maybe_unused]] int argc, [[maybe_unused]] char **argv) {
@@ -49,7 +51,7 @@ SDL_AppResult SDL_AppInit(void **appstate, [[maybe_unused]] int argc, [[maybe_un
 		return SDL_APP_FAILURE;
 	}
 
-	if(!SDL_CreateWindowAndRenderer("physics", WIDTH, HEIGHT, 0, &state->window, &state->renderer)) {
+	if(!SDL_CreateWindowAndRenderer("physics", WIDTH, HEIGHT, SDL_WINDOW_RESIZABLE, &state->window, &state->renderer)) {
 		printf("Failed to create window or renderer: %s", SDL_GetError());
 		return SDL_APP_FAILURE;
 	}
@@ -63,24 +65,25 @@ SDL_AppResult SDL_AppInit(void **appstate, [[maybe_unused]] int argc, [[maybe_un
 
 	state->deltatime = 0;
 
-	state->ball_count = 2000;
+	//state->ball_count = 1000;
+	state->ball_count = 0;
 	//state->ball_count = 130;
 	state->balls = malloc(state->ball_count * sizeof(ball_3d));
 	printf("ball mem usage (kb): %zu\n", state->ball_count * sizeof(ball_3d) / 1000);
-	state->grid = construct_grid_3d(25, 100, 25, 0.2);
+	state->grid = construct_grid_3d(25, 50, 25, 0.3);
 
-	float ball_radius = 0.1;
-	float spawn_height = 3;
+	float ball_radius = 0.15;
+	float spawn_height = 1;
 
 	int x = 0, y = 0, z = 0;
-	int max_width = 10;
+	int max_width = 11;
 
 	for(int i = 0; i < state->ball_count; ++i) {
-		if(x > max_width-1) {
+		if(x >= max_width-1) {
 			x = 0;
 			z++;
 		}
-		if(z > max_width-1) {
+		if(z >= max_width-1) {
 			z = 0;
 			y++;
 		}
@@ -136,6 +139,39 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
 	int steps_per_frame = 5;
 	state->deltatime = 1.0/framerate/steps_per_frame;
 	//state->deltatime = 0;
+	if(key_states[SDL_SCANCODE_RETURN]) {
+		if(SDL_GetTicks() > state->spawn_tick_count + 100) {
+			int spawn_count = 25;
+			int x = 0, y = 0, z = 0;
+			int max_width = 5;
+			float ball_radius = 0.15;
+			float spawn_height = 5;
+			state->spawn_tick_count = SDL_GetTicks();
+			state->balls = realloc(state->balls, (state->ball_count += spawn_count) * sizeof(ball_3d));
+
+			for(int i = 1; i <= spawn_count; ++i) {
+				if(x >= max_width-1) {
+					x = 0;
+					z++;
+				}
+				if(z >= max_width-1) {
+					z = 0;
+					y++;
+				}
+				x++;
+				//state->balls[state->ball_count-1].previous_position = (vec3){0, 10.01, 0};
+				//state->balls[state->ball_count-1].position = (vec3){0, 10, 0};
+				//state->balls[state->ball_count-1].mass = 1;
+				//state->balls[state->ball_count-1].radius = 0.1;
+
+				state->balls[state->ball_count-i].position = (vec3){(x-max_width*0.5) * 2 * ball_radius, spawn_height + y * 2 * ball_radius, (z-max_width*0.5) * 2 * ball_radius};
+				set_velocity_3d(state->balls + state->ball_count - i, (vec3){0, -0.03, 0});
+				state->balls[state->ball_count - i].mass = 1;
+				state->balls[state->ball_count - i].radius = ball_radius;
+				//state->balls[state->ball_count - i].radius = (rand() % 5 + 10)* 0.01;
+			}
+		}
+	}
 
 	SDL_SetRenderDrawColor(state->renderer, 0, 0, 0, 255);
 	SDL_RenderClear(state->renderer);
@@ -143,84 +179,81 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
 	SDL_SetRenderDrawColor(state->renderer, 255, 0, 0, 1);
 
 
-	mat3 rotation;
-	if(key_states[SDL_SCANCODE_LEFT]) {
-		rotation = generate_rotation_matrix(0, 0.01, 0);
-	} else {
-		rotation = generate_rotation_matrix(0, 0, 0);
-	}
+	mat3 transform;
+	transform = generate_rotation_matrix(0, 0, 0);
+	transform = m3_mult(transform, generate_scale_matrix((vec3){1, 1, 1}));
 
 	wall_3d bottom1 = (wall_3d) {
-		.vertex_a = m3_v3_mult(rotation, (vec3){-2, 0, -2}),
-		.vertex_b = m3_v3_mult(rotation, (vec3){-2, 0, 2}),
-		.vertex_c = m3_v3_mult(rotation, (vec3){2, 0, 2}),
+		.vertex_a = m3_v3_mult(transform, (vec3){-2, 0, -2}),
+		.vertex_b = m3_v3_mult(transform, (vec3){-2, 0, 2}),
+		.vertex_c = m3_v3_mult(transform, (vec3){2, 0, 2}),
 	};
 	set_wall_normal(&bottom1);
 
 	wall_3d bottom2 = (wall_3d) {
-		.vertex_a = m3_v3_mult(rotation, (vec3){-2, 0, -2}),
-		.vertex_b = m3_v3_mult(rotation, (vec3){2, 0, -2}),
-		.vertex_c = m3_v3_mult(rotation, (vec3){2, 0, 2}),
+		.vertex_a = m3_v3_mult(transform, (vec3){-2, 0, -2}),
+		.vertex_b = m3_v3_mult(transform, (vec3){2, 0, -2}),
+		.vertex_c = m3_v3_mult(transform, (vec3){2, 0, 2}),
 	};
 	set_wall_normal(&bottom2);
 
 
 	wall_3d back1 = (wall_3d) {
-		.vertex_a = m3_v3_mult(rotation, (vec3){-2, 0, 2}),
-		.vertex_b = m3_v3_mult(rotation, (vec3){2, 0, 2}),
-		.vertex_c = m3_v3_mult(rotation, (vec3){2, 1, 2}),
+		.vertex_a = m3_v3_mult(transform, (vec3){-2, 0, 2}),
+		.vertex_b = m3_v3_mult(transform, (vec3){2, 0, 2}),
+		.vertex_c = m3_v3_mult(transform, (vec3){2, 1, 2}),
 	};
 	set_wall_normal(&back1);
 
 	wall_3d back2 = (wall_3d) {
-		.vertex_a = m3_v3_mult(rotation, (vec3){-2, 1, 2}),
-		.vertex_b = m3_v3_mult(rotation, (vec3){2, 1, 2}),
-		.vertex_c = m3_v3_mult(rotation, (vec3){-2, 0, 2}),
+		.vertex_a = m3_v3_mult(transform, (vec3){-2, 1, 2}),
+		.vertex_b = m3_v3_mult(transform, (vec3){2, 1, 2}),
+		.vertex_c = m3_v3_mult(transform, (vec3){-2, 0, 2}),
 	};
 	set_wall_normal(&back2);
 
 
 	wall_3d front1 = (wall_3d) {
-		.vertex_a = m3_v3_mult(rotation, (vec3){-2, 0, -2}),
-		.vertex_b = m3_v3_mult(rotation, (vec3){2, 0, -2}),
-		.vertex_c = m3_v3_mult(rotation, (vec3){2, 1, -2}),
+		.vertex_a = m3_v3_mult(transform, (vec3){-2, 0, -2}),
+		.vertex_b = m3_v3_mult(transform, (vec3){2, 0, -2}),
+		.vertex_c = m3_v3_mult(transform, (vec3){2, 1, -2}),
 	};
 	set_wall_normal(&front1);
 
 	wall_3d front2 = (wall_3d) {
-		.vertex_a = m3_v3_mult(rotation, (vec3){-2, 1, -2}),
-		.vertex_b = m3_v3_mult(rotation, (vec3){2, 1, -2}),
-		.vertex_c = m3_v3_mult(rotation, (vec3){-2, 0, -2}),
+		.vertex_a = m3_v3_mult(transform, (vec3){-2, 1, -2}),
+		.vertex_b = m3_v3_mult(transform, (vec3){2, 1, -2}),
+		.vertex_c = m3_v3_mult(transform, (vec3){-2, 0, -2}),
 	};
 	set_wall_normal(&front2);
 
 
 	wall_3d left1 = (wall_3d) {
-		.vertex_a = m3_v3_mult(rotation, (vec3){-2, 0, -2}),
-		.vertex_b = m3_v3_mult(rotation, (vec3){-2, 1, -2}),
-		.vertex_c = m3_v3_mult(rotation, (vec3){-2, 1, 2}),
+		.vertex_a = m3_v3_mult(transform, (vec3){-2, 0, -2}),
+		.vertex_b = m3_v3_mult(transform, (vec3){-2, 1, -2}),
+		.vertex_c = m3_v3_mult(transform, (vec3){-2, 1, 2}),
 	};
 	set_wall_normal(&left1);
 
 	wall_3d left2 = (wall_3d) {
-		.vertex_a = m3_v3_mult(rotation, (vec3){-2, 0, -2}),
-		.vertex_b = m3_v3_mult(rotation, (vec3){-2, 1, 2}),
-		.vertex_c = m3_v3_mult(rotation, (vec3){-2, 0, 2}),
+		.vertex_a = m3_v3_mult(transform, (vec3){-2, 0, -2}),
+		.vertex_b = m3_v3_mult(transform, (vec3){-2, 1, 2}),
+		.vertex_c = m3_v3_mult(transform, (vec3){-2, 0, 2}),
 	};
 	set_wall_normal(&left2);
 
 
 	wall_3d right1 = (wall_3d) {
-		.vertex_a = m3_v3_mult(rotation, (vec3){2, 0, -2}),
-		.vertex_b = m3_v3_mult(rotation, (vec3){2, 1, -2}),
-		.vertex_c = m3_v3_mult(rotation, (vec3){2, 1, 2}),
+		.vertex_a = m3_v3_mult(transform, (vec3){2, 0, -2}),
+		.vertex_b = m3_v3_mult(transform, (vec3){2, 1, -2}),
+		.vertex_c = m3_v3_mult(transform, (vec3){2, 1, 2}),
 	};
 	set_wall_normal(&right1);
 
 	wall_3d right2 = (wall_3d) {
-		.vertex_a = m3_v3_mult(rotation, (vec3){2, 0, -2}),
-		.vertex_b = m3_v3_mult(rotation, (vec3){2, 1, 2}),
-		.vertex_c = m3_v3_mult(rotation, (vec3){2, 0, 2}),
+		.vertex_a = m3_v3_mult(transform, (vec3){2, 0, -2}),
+		.vertex_b = m3_v3_mult(transform, (vec3){2, 1, 2}),
+		.vertex_c = m3_v3_mult(transform, (vec3){2, 0, 2}),
 	};
 	set_wall_normal(&right2);
 
@@ -263,7 +296,7 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
 		for(int i = 0; i < state->ball_count; ++i) {
 			ball_3d *current_ball = state->balls + i;
 			update_ball_3d(current_ball);
-			current_ball->position.y -= 9 * state->deltatime * state->deltatime;
+			current_ball->position.y -= 9.8 * state->deltatime * state->deltatime;
 		}
 		for(int i = 0; i < state->ball_count; ++i) {
 			check_and_resolve_3d(state->balls+i, bottom1, 0, 0, state->deltatime);
@@ -299,8 +332,66 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
 	}
 
 	for(int i = 0; i < state->ball_count; ++i) {
-		draw_circle_3d(state->renderer, state->balls[i], 10, state->cam);
+		draw_circle_3d(state->renderer, state->balls[i], 25, state->cam);
 	}
+	
+	/*
+	for(int i = 0; i < state->grid.depth; ++i) {
+		for(int j = 0; j < state->grid.height; ++j) {
+			for(int k = 0; k < state->grid.width; ++k) {
+				//if(state->grid.partitions[i * state->grid.width * state->grid.height + j * state->grid.width + k].ball_count == 0) continue;
+				vec3 line_1_1 = (vec3){
+					state->grid.element_size*0.5 + state->grid.element_size * (k-state->grid.width * 0.5),
+					state->grid.element_size* (j-state->grid.height*0.5),
+					state->grid.element_size * (i-state->grid.depth*0.5)
+				};
+				vec3 line_1_2 = (vec3){
+					-state->grid.element_size*0.5 + state->grid.element_size * (k-state->grid.width * 0.5),
+					state->grid.element_size * (j-state->grid.height*0.5),
+					state->grid.element_size * (i-state->grid.depth*0.5)
+				};
+				line_1_1 = point_to_screen(line_1_1, state->cam);
+				line_1_2 = point_to_screen(line_1_2, state->cam);
+				if(line_1_1.z < 0 || line_1_2.z < 0) continue;
+
+				SDL_RenderLine(state->renderer, line_1_1.x, line_1_1.y, line_1_2.x, line_1_2.y);
+
+				vec3 line_2_1 = (vec3){
+					state->grid.element_size * (k-state->grid.width * 0.5),
+					-state->grid.element_size*0.5 + state->grid.element_size* (j-state->grid.height*0.5),
+					state->grid.element_size * (i-state->grid.depth*0.5)
+				};
+				vec3 line_2_2 = (vec3){
+					state->grid.element_size * (k-state->grid.width * 0.5),
+					state->grid.element_size*0.5 + state->grid.element_size * (j-state->grid.height*0.5),
+					state->grid.element_size * (i-state->grid.depth*0.5)
+				};
+				line_2_1 = point_to_screen(line_2_1, state->cam);
+				line_2_2 = point_to_screen(line_2_2, state->cam);
+				if(line_2_1.z < 0 || line_2_2.z < 0) continue;
+
+				SDL_RenderLine(state->renderer, line_2_1.x, line_2_1.y, line_2_2.x, line_2_2.y);
+
+				vec3 line_3_1 = (vec3){
+					state->grid.element_size * (k-state->grid.width * 0.5),
+					state->grid.element_size* (j-state->grid.height*0.5),
+					-state->grid.element_size * 0.5 + state->grid.element_size * (i-state->grid.depth*0.5)
+				};
+				vec3 line_3_2 = (vec3){
+					state->grid.element_size * (k-state->grid.width * 0.5),
+					state->grid.element_size* (j-state->grid.height*0.5),
+					state->grid.element_size * 0.5 + state->grid.element_size * (i-state->grid.depth*0.5)
+				};
+				line_3_1 = point_to_screen(line_3_1, state->cam);
+				line_3_2 = point_to_screen(line_3_2, state->cam);
+				if(line_3_1.z < 0 || line_3_2.z < 0) continue;
+
+				SDL_RenderLine(state->renderer, line_3_1.x, line_3_1.y, line_3_2.x, line_3_2.y);
+			}
+		}
+	}
+	*/
+	
 
 	draw_wall_3d(state->renderer, bottom1, state->cam);
 	draw_wall_3d(state->renderer, bottom2, state->cam);
@@ -310,6 +401,7 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
 
 	draw_wall_3d(state->renderer, front1, state->cam);
 	draw_wall_3d(state->renderer, front2, state->cam);
+
 
 	draw_wall_3d(state->renderer, left1, state->cam);
 	draw_wall_3d(state->renderer, left2, state->cam);
@@ -328,9 +420,31 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
 
 	if(SDL_GetTicks() > state->frame_tick_count + 1000) {
 		printf("framerate: %i\nball count: %i\n", state->frame_count, state->ball_count);
+		if(state->frame_count < 60) {
+			printf("wee woo wee woo\n");
+		}
 		state->frame_tick_count = SDL_GetTicks();
 		state->frame_count = 0;
+
+		//spatial_partition *bin_partition = state->grid.partitions + state->grid.width * state->grid.height * state->grid.depth;
+		int push_back = 0;
+		for(int i = 0; i < state->ball_count; ++i) {
+			ball_3d *ball = state->balls + i;
+			int ball_column = ball->position.x / state->grid.element_size + state->grid.width * 0.5;
+			int ball_row = ball->position.y / state->grid.element_size + state->grid.height * 0.5;
+			int ball_layer = ball->position.z / state->grid.element_size + state->grid.depth * 0.5;
+
+			state->balls[i-push_back] = state->balls[i];
+
+			if(ball_column < 0 || ball_column > state->grid.width - 1
+				|| ball_row < 0 || ball_row > state->grid.height - 1
+				|| ball_layer < 0 || ball_layer > state->grid.depth - 1)
+				++push_back;
+		}
+		state->ball_count -= push_back;
+		state->balls = realloc(state->balls, sizeof(ball_3d) * state->ball_count);
 	}
+
 
 
 	return SDL_APP_CONTINUE;
